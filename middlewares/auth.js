2479fce7776c4d6d1ec12a6f7442960a5cdb41ba
antunesdvz <g.2008.antunes@gmail.com>
2025-09-19 17:15:37 -0300

    Alteração 19/09/2025 17:15

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,10 @@
 export function verifyUser(req, res, next) {
     console.log("Passei no middleware")
 
+    const headers =req.headers;
+    console.log(headers);
+
+
     const auth = req.headers.authorization;
     console.log(auth);  
 
@@ -11,13 +15,26 @@ export function verifyUser(req, res, next) {
     }
     //pegar conteudo encriptado
 
-    const conteudo_do_token = auth.split(' ')
+    const conteudo_do_token = auth.split(' ')[1];
     console.log(conteudo_do_token);
 
     //desemcriptar o conteudo 
+    const token_descriptografado = Buffer .from(
+        conteudo_do_token,
+        'base64' //tipo de encriptacao
+    ).toString('utf-8');
+
+    console.log(token_descriptografado);
 
     //tendo usuario e senha
+
+    const usuario = token_descriptografado.split(':')[0];
+
+
     //preciso verificar se o usuario existe no banco
+    //chamem o findUnique do prisma, passansdo o nome
+
+    //const usuario = await prisma.users.findUnique({where: { id: id}});
 
     //se nao existir, nao pode acessar
 }
